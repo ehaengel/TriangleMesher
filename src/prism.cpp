c595a8ddb7990ffce247aa1492b9977bc688c13e
Eric Haengel <eric.c.haengel@gmail.com>
2013-03-15 02:18:02 -0400

    I forgot to check in the prism code before

---
+++ src/prism.cpp
@@ -0,0 +1,89 @@
+#include "prism.h"
+
+Prism::Prism() {
+	min = Vector2d(0.0, 0.0);
+	max = Vector2d(0.0, 0.0);
+}
+
+Prism::~Prism() {
+	//Do nothing
+}
+
+int Prism::SetMin(Vector2d min) {
+	this->min = min;
+	return true;
+}
+
+int Prism::SetMax(Vector2d max) {
+	this->max = max;
+	return true;
+}
+
+int Prism::SetMinMax(Vector2d min, Vector2d max) {
+	this->min = min;
+	this->max = max;
+
+	return true;
+}
+
+Vector2d Prism::GetMin() {
+	return min;
+}
+
+Vector2d Prism::GetMax() {
+	return max;
+}
+
+//Check if the point is inside the open/closed prism
+int Prism::TestPointInside(Vector2d pt, int soft_edges) {
+	//Test if the point is inside the open prism
+	if(soft_edges == true) {
+		if(pt.x <= min.x - EFF_ZERO || pt.x >= max.x + EFF_ZERO)
+			return false;
+
+		if(pt.y <= min.y - EFF_ZERO || pt.y >= max.y + EFF_ZERO)
+			return false;
+	}
+
+	//Test if the point is inside the closed prism
+	else {
+		if(pt.x < min.x - EFF_ZERO || pt.x > max.x + EFF_ZERO)
+			return false;
+
+		if(pt.y < min.y - EFF_ZERO || pt.y > max.y + EFF_ZERO)
+			return false;
+	}
+
+	//The point was inside the prism
+	return true;
+}
+
+//Expand the prism to include this point
+int Prism::Expand(Vector2d pt) {
+	if(pt.x < min.x)
+		min.x = pt.x;
+
+	if(pt.y < min.y)
+		min.y = pt.y;
+
+	if(pt.x > max.x)
+		max.x = pt.x;
+
+	if(pt.y > max.y)
+		max.y = pt.y;
+
+	return true;
+}
+
+int Prism::write_svg(FILE* handle, double w, double h) {
+	double x = min.x;
+	double y = h - max.y;
+
+	double width = (max.x - min.x);
+	double height = (max.y - min.y);
+
+	fprintf(handle, "<rect x=\"%f\" y=\"%f\" width=\"%f\" height=\"%f\" ", x, y, width, height);
+	fprintf(handle, "fill=\"none\" stroke-width=\"5\" stroke=\"red\"/>\n");
+
+	return true;
+}
---
+++ src/prism.h
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <vector>
+using namespace std;
+
+#include "vector2d.h"
+#include "utility.h"
+
+#ifndef PRISM
+#define PRISM
+
+class Prism {
+public:
+	Prism();
+	~Prism();
+
+	/////////////////////
+	// Data management //
+	/////////////////////
+
+	int SetMin(Vector2d min);
+	int SetMax(Vector2d max);
+	int SetMinMax(Vector2d min, Vector2d max);
+
+	Vector2d GetMin();
+	Vector2d GetMax();
+
+	//////////////////////////
+	// Geometric primitives //
+	//////////////////////////
+
+	//Check if the point is inside the open/closed prism
+	int TestPointInside(Vector2d pt, int soft_edges);
+
+	//Expand the prism to include this point
+	int Expand(Vector2d pt);
+
+	/////////////////////////
+	// Debugging functions //
+	/////////////////////////
+
+	int write_svg(FILE* handle, double w, double h);
+
+private:
+	Vector2d min, max;
+};
+
+typedef vector<Prism*> PrismList;
+
+#endif
